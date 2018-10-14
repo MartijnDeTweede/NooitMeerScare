5bdb1ac2f7acd7237eb4d0c87bbbd2f10de346f6
MartijnSchotel <martijn.schotel@eneco.com>
2018-10-14 16:46:06 +0200

    basis for table added

--- nooit-meer-scare/src/Components/MonthlyBalanceTool/MonthlyBalanceTool.jsx
+++ nooit-meer-scare/src/Components/MonthlyBalanceTool/MonthlyBalanceTool.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ExpensesModal from '../ExpensesModal/ExpensesModal';
 import MajorButton from '../MajorButton/MajorButton';
+import Expenses from './expenses.json';
+import './monthlyBalanceTool.css';
+import HeaderData from './headers.json';
 
 class MonthlyBalanceTool extends Component {
   constructor(props) {
@@ -8,6 +11,7 @@ class MonthlyBalanceTool extends Component {
 
     this.state = {
       ExpensesModalOpen: false,
+      expenses: Expenses,
     }
   }
   openModal = (modalname) => {
@@ -18,12 +22,42 @@ class MonthlyBalanceTool extends Component {
     this.setState({ [modalname]: false });
   }
 
+  renderHeaders = () => (
+    <tr>
+      {HeaderData.map(header => (
+        <td>
+          <span >{header.value}</span>
+        </td>        
+      ))}
+    </tr>
+  )
+
+  renderExpenses = () => {
+    const filtedExpenses = this.state.expenses.filter(expense => expense.selected);
+    return (
+    <table>
+      {this.renderHeaders()}
+      {filtedExpenses.map(expense => (
+        <tr>
+          <td>
+            <span>{expense.catagory}</span>
+          </td>
+          <td>
+            <span>{expense.subcatagory}</span>
+          </td>
+          {expense.values.map(value => (<td>€ {value.ammount}</td>))}
+        </tr>
+      ))}
+    </table>
+  )}
+
   render() {
     const { ExpensesModalOpen } = this.state;
 
     return (
       <div>
         Dit is de monthlyBalancetool
+        {this.renderExpenses()}
         <MajorButton text="Kies uitgaven" onClick={() => {this.openModal('ExpensesModalOpen')}} />
         <ExpensesModal isOpen={ExpensesModalOpen} closeModal={() => {this.closeModal('ExpensesModalOpen')}}/>
       </div>
